b222150cd311231238f8efa20f4d21f5d67c1b7f
vadim <vadim@192.168.152.42>
2018-05-16 14:56:46 +0300

    New Commit

---
+++ src/main/java/com/nvs/htmlparsertest/Main.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nvs.htmlparsertest;
+
+/**
+ *
+ * @author vadim
+ */
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Document doc;
+        try {
+
+            // need http protocol
+            doc = Jsoup.connect("https://www.ikea.com/pl/pl/catalog/products/20343224/").get();
+
+            // get page title
+            String title = doc.title();
+            System.out.println("title : " + title);
+
+            // get all links
+            Elements links = doc.select("a[href]");
+            for (Element link : links) {
+
+                // get the value from href attribute
+                System.out.println("\nlink : " + link.attr("href"));
+                System.out.println("text : " + link.text());
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
